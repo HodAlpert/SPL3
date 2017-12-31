389700262cdc14e3bc8de717f73885439823f0bd
hod <hod246>
2017-12-31 18:31:21 +0200

    connections

---
+++ src/main/java/bgu/spl181/net/impl/ClientForConnections.java
@@ -0,0 +1,32 @@
+package bgu.spl181.net.impl;
+
+import bgu.spl181.net.srv.ConnectionHandler;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class ClientForConnections {
+
+    private final int id;
+    private AtomicBoolean isLoggedIn;
+    private final ConnectionHandler handler;
+
+    public ClientForConnections(int id, ConnectionHandler handler){
+        this.id=id;
+        isLoggedIn = new AtomicBoolean(false);
+        this.handler=handler;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public ConnectionHandler getHandler() {
+        return handler;
+    }
+    public boolean isLoogedin(){
+        return isLoggedIn.get();
+    }
+    public void setIsLoggedIn(boolean value){
+        isLoggedIn.set(value);
+    }
+
+}
---
+++ src/main/java/bgu/spl181/net/impl/ServerConnections.java
@@ -0,0 +1,44 @@
+package bgu.spl181.net.impl;
+
+import bgu.spl181.net.api.bidi.Connections;
+import bgu.spl181.net.srv.ConnectionHandler;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ServerConnections<T> implements Connections<T> {
+    private ConcurrentHashMap<Integer,ClientForConnections> map;
+
+    public ServerConnections() {
+        this.map = new ConcurrentHashMap<Integer,ClientForConnections>();
+    }
+
+    @Override
+    public boolean send(int connectionId, T msg) {
+        ClientForConnections client = map.get(connectionId);
+        boolean answer =(client!=null);
+        if (answer){
+            ConnectionHandler handler= map.get(connectionId).getHandler();
+            handler.send(msg);
+        }
+        return answer;
+    }
+
+    @Override
+    public void broadcast(T msg) {
+        for(ClientForConnections client : map.values()){
+            client.getHandler().send(msg);
+        }
+    }
+
+    @Override
+    public void disconnect(int connectionId) {
+        map.remove(connectionId);//TODO make sure connection handler closes itself
+    }
+    public void activate(int connectionId,ConnectionHandler handler){
+        ClientForConnections client = new ClientForConnections(connectionId,handler);
+        map.putIfAbsent(connectionId,client);
+    }
+    public void connect(int connectionId){
+        map.get(connectionId).setIsLoggedIn(true);
+    }
+}

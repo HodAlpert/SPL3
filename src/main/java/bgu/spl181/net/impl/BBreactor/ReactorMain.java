3e1ca886463fc787295968761eb448a6bd1056cb
hod <hod246>
2018-01-01 10:55:22 +0200

    connections

--- src/main/java/bgu/spl181/net/impl/BBreactor/ReactorMain.java
+++ src/main/java/bgu/spl181/net/impl/BBreactor/ReactorMain.java
@@ -1,4 +1,25 @@
 package bgu.spl181.net.impl.BBreactor;
 
+import bgu.spl181.net.api.bidi.BidiMessagingProtocol;
+import bgu.spl181.net.impl.BidiProtocol;
+import bgu.spl181.net.impl.echo.LineMessageEncoderDecoder;
+import bgu.spl181.net.srv.Reactor;
+
 public class ReactorMain {
-}
+    public static void main(String[] args) {
+
+
+        Reactor<String> server = new Reactor<>(
+                6,
+                1000, //The port
+
+                BidiProtocol<String>::new, //protocol factory
+
+                LineMessageEncoderDecoder::new);
+         //encdec factory
+
+        server.serve();
+
+    }}
+
+

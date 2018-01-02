010de8e0f91ccbc2e9d360e4ec236e47cc0f7270
hod <hod246>
2018-01-02 21:48:24 +0200

    User added

--- src/main/java/bgu/spl181/net/impl/BBtpc/MovieRentalTPCServer.java
+++ src/main/java/bgu/spl181/net/impl/BBtpc/MovieRentalTPCServer.java
@@ -2,14 +2,18 @@ package bgu.spl181.net.impl.BBtpc;
 
 import bgu.spl181.net.api.MessageEncoderDecoder;
 import bgu.spl181.net.api.bidi.BidiMessagingProtocol;
+import bgu.spl181.net.impl.MovieRentalService;
 import bgu.spl181.net.srv.BaseServer;
 import bgu.spl181.net.srv.BlockingConnectionHandler;
 
 import java.util.function.Supplier;
 
-public class TPCServer<T> extends BaseServer<T> {
-    public TPCServer(int port, Supplier<BidiMessagingProtocol<T>> protocolFactory, Supplier<MessageEncoderDecoder<T>> encdecFactory) {
+public class MovieRentalTPCServer<T> extends BaseServer<T> {
+    private MovieRentalService service;
+
+    public MovieRentalTPCServer(int port, Supplier<BidiMessagingProtocol<T>> protocolFactory, Supplier<MessageEncoderDecoder<T>> encdecFactory) {
         super(port, protocolFactory, encdecFactory);
+        service = new MovieRentalService();
     }
 
     @Override
--- src/main/java/bgu/spl181/net/impl/BBtpc/TPCMain.java
+++ src/main/java/bgu/spl181/net/impl/BBtpc/TPCMain.java
@@ -5,7 +5,7 @@ import bgu.spl181.net.impl.echo.LineMessageEncoderDecoder;
 
 public class TPCMain {
     public static void main(String[] args) {
-        TPCServer<String> server = new TPCServer<>(
+        MovieRentalTPCServer<String> server = new MovieRentalTPCServer<>(
                 Integer.parseInt(args[0]),
                 BidiProtocol::new,
                 LineMessageEncoderDecoder::new);

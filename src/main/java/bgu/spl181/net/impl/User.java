98e9d018f700d36693cbf9b5e8e1168bc84e1c11
hod <hod246>
2018-01-02 21:11:50 +0200

    User added

---
+++ src/main/java/bgu/spl181/net/impl/User.java
@@ -0,0 +1,70 @@
+package bgu.spl181.net.impl;
+
+import java.util.ArrayList;
+
+public class User {
+    private String username;
+    private String type;
+    private String country;
+    private ArrayList<String> movies;
+    private int balance;
+
+    public User(String username, String type, String country, int balance) {
+        this.username = username;
+        this.type = type;
+        this.country = country;
+        this.balance = balance;
+        this.movies = new ArrayList<String>();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public ArrayList<String> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(ArrayList<String> movies) {
+        this.movies = movies;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", type='" + type + '\'' +
+                ", country='" + country + '\'' +
+                ", movies=" + movies +
+                ", balance=" + balance +
+                '}';
+    }
+}

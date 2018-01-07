679a2f6d75952fe86235020a6b3fb79176550716
hod <hod246>
2018-01-07 16:39:24 +0200

    updating

--- src/main/java/bgu/spl181/net/impl/BBreactor/ReactorMain.java
+++ src/main/java/bgu/spl181/net/impl/BBreactor/ReactorMain.java
@@ -1,21 +1,22 @@
-//package bgu.spl181.net.impl.BBreactor;
-//
-//import bgu.spl181.net.api.DataHandler;
-//import bgu.spl181.net.impl.protocol.MovieRentalProtocol;
-//import bgu.spl181.net.impl.DataHandling.MovieRentalService;
-//import bgu.spl181.net.impl.encoder.LineMessageEncoderDecoder;
-//import bgu.spl181.net.srv.Reactor;
-//
-//public class ReactorMain {
-//    public static void main(String[] args) {
-//        DataHandler service= new MovieRentalService();
-//               Reactor<String> server = new MovieRentalReactor(
-//                10,//num of threads
-//                       Integer.parseInt(args[0]),//the port
-//                       ()->new MovieRentalProtocol((MovieRentalService) service), //protocol factory
-//                LineMessageEncoderDecoder::new);
-//        server.serve();
-//    }
-//}
-//
-//
+package bgu.spl181.net.impl.BBreactor;
+
+import bgu.spl181.net.api.DataHandler;
+import bgu.spl181.net.impl.messages.Message;
+import bgu.spl181.net.impl.protocol.MovieRentalProtocol;
+import bgu.spl181.net.impl.DataHandling.MovieRentalService;
+import bgu.spl181.net.impl.encoder.LineMessageEncoderDecoder;
+import bgu.spl181.net.srv.Reactor;
+
+public class ReactorMain {
+    public static void main(String[] args) {
+        DataHandler<Message> service= new MovieRentalService();
+               Reactor<Message> server = new Reactor<>(
+                10,//num of threads
+                       Integer.parseInt(args[0]),//the port
+                       ()->new MovieRentalProtocol((MovieRentalService) service), //protocol factory
+                LineMessageEncoderDecoder::new);
+        server.serve();
+    }
+}
+
+
--- src/main/java/bgu/spl181/net/impl/BBtpc/MovieRentalTPCServer.java
+++ src/main/java/bgu/spl181/net/impl/BBtpc/MovieRentalTPCServer.java
@@ -2,14 +2,15 @@ package bgu.spl181.net.impl.BBtpc;
 
 import bgu.spl181.net.api.MessageEncoderDecoder;
 import bgu.spl181.net.api.bidi.BidiMessagingProtocol;
+import bgu.spl181.net.impl.messages.Message;
 import bgu.spl181.net.srv.BaseServer;
 import bgu.spl181.net.srv.BlockingConnectionHandler;
 
 import java.util.function.Supplier;
 
-public class MovieRentalTPCServer extends BaseServer<String> {
+public class MovieRentalTPCServer extends BaseServer<Message> {
 
-    public MovieRentalTPCServer(int port, Supplier<BidiMessagingProtocol<String>> protocolFactory, Supplier<MessageEncoderDecoder<String>> encdecFactory) {
+    public MovieRentalTPCServer(int port, Supplier<BidiMessagingProtocol<Message>> protocolFactory, Supplier<MessageEncoderDecoder<Message>> encdecFactory) {
         super(port, protocolFactory, encdecFactory);
     }
 
